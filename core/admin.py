5b16ea594e2e46d27c058b2c50fc609c36ede441
Richelly Italo <richelly@ponteon.com.br>
2021-04-17 14:09:45 -0300

    ✨ Banner model

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Post, Page
+from .models import Post, Page, Banner
 
 # Register your models here.
 
+
+class BannerAdmin(admin.ModelAdmin):
+    exclude = ('sort_order', )
+
+
 admin.site.register(Post)
 admin.site.register(Page)
+admin.site.register(Banner, BannerAdmin)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, pre_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 import os
@@ -32,6 +32,28 @@ class Page(TimestampableMixin):
 
     def __str__(self):
         return self.title
+
+
+class Banner(TimestampableMixin):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='image/%Y/%m/%d/')
+    sort_order = models.PositiveIntegerField(default=1)
+    url = models.CharField(max_length=400, blank=True)
+    new_tab = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    
+    def __str__(self):
+        return self.title
+
+
+@receiver(pre_save, sender=Banner)
+def handler_set_sorter_pre_save(sender, instance, **kwargs):
+    if instance._state.adding:
+        last_banner = Banner.objects.order_by('-pk')[:1]
+        if last_banner.exists():
+            instance.sort_order = last_banner[0].sort_order + 1
 
 
 @receiver(post_delete, sender=Post)
