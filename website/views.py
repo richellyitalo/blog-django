c61eb2a2d686a364e357c4a0acc9a24c60626332
Richelly Italo <richellyitalo@gmail.com>
2021-04-21 00:03:15 -0300

    ✨ Route posts with custom limit

--- website/views.py
+++ website/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import pagination
 
 from core.models import Post
 from core.serializers import PostSerializer
 
 
-class PostList(generics.ListAPIView):
-    queryset = Post
-    serializer = PostSerializer
+class PublicPagination(pagination.LimitOffsetPagination):
+    default_limit = 12
+
+
+class PublicRoute(generics.GenericAPIView):
+    permission_classes = []
+    pagination_class = PublicPagination
+
+
+class PostList(PublicRoute, generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
